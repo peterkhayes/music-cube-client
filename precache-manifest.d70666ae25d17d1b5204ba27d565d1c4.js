be1c7e6975d49f0c4fa41522e43aa159efe47181
Peter Hayes <peter.k.hayes@gmail.com>
2020-03-23 09:46:25 -0700

    Updates

---
+++ precache-manifest.d70666ae25d17d1b5204ba27d565d1c4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b209dcb3bbcf5138e63e793c9fd4c26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89854533209dbbafea5f",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0e66a12a4b86c7a5031e",
+    "url": "/static/js/2.b2430bd9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b2430bd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89854533209dbbafea5f",
+    "url": "/static/js/main.28a2c7dc.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "5d7f897cdb7861f7783762cef1e77921",
+    "url": "/static/media/back.5d7f897c.wav"
+  },
+  {
+    "revision": "d085edae00e13d18a57ea7c7d18c7d76",
+    "url": "/static/media/down.d085edae.wav"
+  },
+  {
+    "revision": "31e938c8efa8307023788c12aa29c6a6",
+    "url": "/static/media/front.31e938c8.wav"
+  },
+  {
+    "revision": "d49811147161e0627be8c10249480180",
+    "url": "/static/media/left.d4981114.wav"
+  },
+  {
+    "revision": "6b4030e7209a4752fd43635cfa2aca27",
+    "url": "/static/media/right.6b4030e7.wav"
+  },
+  {
+    "revision": "4a48a97a11092e562086079ada88a757",
+    "url": "/static/media/up.4a48a97a.wav"
+  }
+]);
